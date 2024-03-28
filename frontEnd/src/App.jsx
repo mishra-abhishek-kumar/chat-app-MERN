3df934be24370fd136f9c9a09795c747df75b836
mishra_abhishek_kumar <mishraabhishek0807@gmail.com>
2024-03-29 00:03:34 +0530

    created sidebar chat section

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import SignUp from "./pages/signup/SignUp";
+import Home from "./pages/home/Home";
 
 function App() {
 	return (
 		<div className="p-4 h-screen flex items-center justify-center">
-			<SignUp />
+			<Home />
 		</div>
 	);
 }
